eb2395bf28e1235241608d9d0e3a7b1ce7d5939d
Charles Humphrey <charles.humphrey35@yahoo.co.uk>
2019-10-10 10:52:02 +0100

    Added another project for a community post regarding 2D flag shaders.

---
+++ Sandboxs/Community/CommunityPost11895/Content/Shaders/flag2D.fx
@@ -0,0 +1,38 @@
+﻿
+float Time;
+
+float2 amplitude = float2(.01f, .1f);
+float2 frequency = float2(12, 8);
+
+Texture2D Texture;
+sampler TextureSampler
+{
+	Texture = <Texture>;
+};
+
+struct vsOutput
+{
+	float4 position : SV_Position;
+	float4 color : COLOR0;
+	float2 texCoord : TEXCOORD0;
+};
+
+float2 CalcUVAnimation(float2 Offset)
+{
+	return float2 ((sin(Time + Offset.y * frequency.x) * amplitude.x) + Offset.x, (cos(Time + Offset.x * frequency.y) * amplitude.y) + Offset.y);
+}
+
+float4 main(vsOutput input) : COLOR0
+{
+	float2 uv = CalcUVAnimation(input.texCoord);
+	float4 color = tex2D(TextureSampler, uv);
+	return color;
+}
+
+technique Falg
+{
+	pass Pass1
+	{
+		PixelShader = compile ps_4_0 main();
+	}
+}
---
+++ Sandboxs/Community/CommunityPost11895/Game1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CommunityPost11895
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Effect effect;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.GraphicsProfile = GraphicsProfile.HiDef;
+
+            graphics.PreferredBackBufferWidth = 1920;
+            graphics.PreferredBackBufferHeight = 1080;
+
+            IsMouseVisible = true;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //effect = Content.Load<Effect>("Shaders/flag2D");
+            effect = Content.Load<Effect>("Shaders/flag2D");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+            effect.CurrentTechnique.Passes[0].Apply();
+            effect.Parameters["Time"].SetValue((float)gameTime.TotalGameTime.TotalMilliseconds * .01f);
+            spriteBatch.Draw(Content.Load<Texture2D>("Textures/UK"), new Rectangle(0, 0, 640, 320), Color.White);
+            spriteBatch.Draw(Content.Load<Texture2D>("Textures/EU"), new Rectangle(640, 0, 640, 320), Color.White);
+            spriteBatch.Draw(Content.Load<Texture2D>("Textures/US"), new Rectangle(1280, 0, 640, 320), Color.White);
+            spriteBatch.End();
+        }
+    }
+}
